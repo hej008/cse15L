a6025607e6740494db70758902f1ec7dd73583a3
HEPU <hej008@ucsd.edu>
2018-02-07 17:08:48 -0800

    Add default value/comments

--- Hello.java
+++ Hello.java
@@ -2,7 +2,10 @@ public class Hello
 {
   public static void main(String argv[])
     {
-      System.out.println("Hello, " + argv[0] + "!");
+	// default is World
+	String name = "World";
+	if (argv.length != 0) name = argv[0];
+      System.out.println("Hello, " + name + "!");
     }
 }
 
