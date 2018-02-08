ebc2a49e65b2481e1688f89746d6741e7bb50cf2
HEPU <hej008@ucsd.edu>
2018-02-07 17:23:14 -0800

    Add an author/email

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello
   public static void main(String argv[])
     {
 	// default is World
+	// Author: Jim Weirich
 	String name = "World";
 	if (argv.length != 0) name = argv[0];
       System.out.println("Hello, " + name + "!");
